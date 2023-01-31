8976cad2dd7965ff848e504d1ebefa163681f35d
rodrij545 <rodrij545@medtronic.com>
2023-01-31 09:15:21 -0500

    Adición del entry point

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import PyPDF2
-import os
 import helper.options as op
 
 
+if __name__ == '__main__':
+    option  = input("""
+                    Ingrese el número de la opción a utilizar:
+                    1. Automatica - EL orden de lso PDF será el orden ene el qeu se enceuntren en la carpeta Documents
+                    2. Manual - Ingrese manualmente lso docuemntos en el orden que desea que se agrupen
+                    Presione enter sin ingresar ninguan opción pada terminar la ejecución
+                    Ingrese la opción: """)
 
-option  = input("""
-                Ingrese el número de la opción a utilizar:
-                1. Automatica - EL orden de lso PDF será el orden ene el qeu se enceuntren en la carpeta Documents
-                2. Manual - Ingrese manualmente lso docuemntos en el orden que desea que se agrupen
-                Presione enter sin ingresar ninguan opción pada terminar la ejecución
-                Ingrese la opción: """)
-
-if option == '1':
-    op.automaticOption()
-elif option == '2':
-    op.manualOption()
+    if option == '1':
+        op.automaticOption()
+    elif option == '2':
+        op.manualOption()
